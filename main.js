c592ac17d4f4efb80727fe34f3c25cc47470fe5c
Пользователь <asadpro888>
2024-02-07 21:39:13 +0300

    goal for

---
+++ main.js
@@ -0,0 +1,23 @@
+const imageContainerEl = document.querySelector(".image-container");
+
+const prevEl = document.getElementById("prev");
+const nextEl = document.getElementById("next");
+let x = 0;
+prevEl.addEventListener("click", ()=>{
+    x = x + 45;
+    updateGallery()
+});
+nextEl.addEventListener("click", ()=>{
+    x = x - 45;
+    updateGallery()
+});
+
+function updateGallery(){
+    imageContainerEl.style.transform = `perspective(1000px) rotateY(${x}deg)`;
+    setTimeout(()=>{
+        x = x -45
+        updateGallery()
+    }, 6000)
+}
+
+updateGallery()
